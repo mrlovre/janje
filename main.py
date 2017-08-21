484e6fd437d9cc875178a740d87db48616d6710d
Lovre Mrčela <lovre.mrcela@fer.hr>
2017-08-21 23:19:39 +0200

    GUI improvement.

--- main.py
+++ main.py
@@ -8,31 +8,24 @@ import pyquery as pq
 current_no = 1
 
 
+def set_current_no(new_value):
+    if new_value is not None and 1 <= new_value <= 151:
+        global current_no
+        current_no = new_value
+
+
 def main():
     win = tk.Tk()
-
-    def btn_callback(btn):
-
-        def callback():
-            global current_no
-            if btn['text'] == '<':
-                if current_no > 1:
-                    current_no -= 1
-            elif btn['text'] == '>':
-                if current_no < 151:
-                    current_no += 1
-            else:
-                pass
-            refresh_gui()
-
-        return callback
-
+    win.wm_title('Pokedex')
     btn_prev = tk.Button(win, text='<')
-    btn_prev['command'] = btn_callback(btn_prev)
     btn_prev.pack(side='left')
     btn_next = tk.Button(win, text='>')
-    btn_next['command'] = btn_callback(btn_next)
     btn_next.pack(side='right')
+    fr_selector = tk.Frame(win)
+    ent_num = tk.Entry(fr_selector, width=5)
+    ent_num.pack(side='left')
+    btn_go = tk.Button(fr_selector, border=3, text='Go!')
+    btn_go.pack(side='left')
     canvas = tk.Canvas(win, relief='sunken', width='4c', height='4c', border=5)
     canvas.pack(side='top')
     o = canvas['border']
@@ -42,7 +35,29 @@ def main():
     lbl_name.pack(side='top')
     msg_desc = tk.Message(win, width='4c')
     msg_desc.pack(side='top')
+    fr_selector.pack(side='top')
     im_pokemon = None
+
+    def btn_callback(btn):
+
+        def callback():
+            if btn['text'] == '<':
+                set_current_no(current_no - 1)
+            elif btn['text'] == '>':
+                set_current_no(current_no + 1)
+            elif btn['text'] == 'Go!':
+                value = try_parse(ent_num.get(), int)
+                set_current_no(value)
+                ent_num.delete(0, 'end')
+            else:
+                return
+            refresh_gui()
+
+        return callback
+
+    btn_prev['command'] = btn_callback(btn_prev)
+    btn_next['command'] = btn_callback(btn_next)
+    btn_go['command'] = btn_callback(btn_go)
 
     def refresh_gui():
         name, desc = scrape_data(get_url(current_no), ['name', 'desc'])
@@ -52,6 +67,13 @@ def main():
         im_pokemon = open_image(get_image_url(current_no), size=im_size)
         canvas.delete()
         show_image(canvas, im_pokemon, canvas_image)
+        btn_prev['state'] = 'normal'
+        btn_next['state'] = 'normal'
+        if current_no <= 1:
+            btn_prev['state'] = 'disabled'
+        elif current_no >= 151:
+            btn_next['state'] = 'disabled'
+        win.pack_slaves()
 
     refresh_gui()
     win.mainloop()
@@ -81,14 +103,21 @@ def scrape_data(url, what) -> tuple:
         name = document('.dextab').eq(0).find('table td').eq(1)('font b').html().strip()
         rval += name,
     if 'desc' in what:
-        desc = document('b:contains("Flavour Text")').parents('table').eq(1).find('tr').eq(3).find('td').eq(
-            1).html().strip()
+        desc = (document('b:contains("Flavour Text")')
+                .parents('table').eq(1).find('tr').eq(3).find('td').eq(1).html().strip())
         rval += desc,
     return rval
 
 
 def get_image_url(no):
     return 'http://www.serebii.net/art/th/{}.png'.format(no)
+
+
+def try_parse(s, f):
+    try:
+        return f(s)
+    except ValueError:
+        return None
 
 
 if __name__ == '__main__':
